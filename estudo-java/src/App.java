cfeb0d7c649c9c71644faab62abc5480846caee2
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-06-12 14:19:37 -0300

    readme
    reamde

---
+++ estudo-java/src/App.java
@@ -0,0 +1,10 @@
+
+
+public class App {
+    public static void main(String[] args) {
+        
+        double valorDouble = 9.99;
+        int valorInt = (int) valorDouble;
+        System.out.println( valorInt);
+    }
+}
